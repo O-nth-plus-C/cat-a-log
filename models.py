20119ee5a3d46ae307e00b07c3f6ff001bf2925f
Cadwynne nth <Cadwynnenth@gmail.com>
2019-01-14 13:19:22 -0800

    Add serialize property to Category and Item

--- models.py
+++ models.py
@@ -21,6 +21,15 @@ class Category(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   user = relationship(User)
 
+  @property
+  def serialize(self):
+      #Returns data in easily serializable format
+      return {
+        'category_name' : self.category_name,
+        'category_image'   : self.category_image,
+        'id' : self.id,
+        'user_id' : self.user_id
+      }
 
 class Item(Base):
   __tablename__ = 'items'
@@ -37,7 +46,18 @@ class Item(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   user = relationship(User)
 
-
+  @property
+  def serialize(self):
+      #Returns data in easily serializable format
+      return {
+        'item_name' : self.item_name,
+        'item_image'   : self.item_image,
+        'item_price' : self.item_price,
+        'item_description' : self.item_description,
+        'category_id' : self.category_id,
+        'id' : self.id,
+        'user_id' : self.user_id
+      }
 
 engine = create_engine('sqlite:///catalog.db')
 
