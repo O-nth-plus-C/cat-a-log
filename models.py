7e5ad221c25b81312dfd961b2efbf3e92f96d618
Cadwynne nth <Cadwynnenth@gmail.com>
2019-01-14 14:17:37 -0800

    updated models to pep8

--- models.py
+++ models.py
@@ -5,59 +5,62 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-  __tablename__ = 'users'
 
-  id = Column(Integer, primary_key = True)
-  name = Column(String(250), nullable = False)
-  email = Column(String(250), nullable = False)
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
 
 class Category(Base):
-  __tablename__ = 'categories'
-  id = Column(Integer, primary_key = True)
-  category_name = Column(String)
-  category_image = Column(String)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  user = relationship(User)
+    __tablename__ = 'categories'
 
-  @property
-  def serialize(self):
-      #Returns data in easily serializable format
-      return {
-        'category_name' : self.category_name,
-        'category_image'   : self.category_image,
-        'id' : self.id,
-        'user_id' : self.user_id
-      }
+    id = Column(Integer, primary_key=True)
+    category_name = Column(String)
+    category_image = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        # Returns data in easily serializable format
+        return {
+            'category_name': self.category_name,
+            'category_image': self.category_image,
+            'id': self.id,
+            'user_id': self.user_id
+            }
+
 
 class Item(Base):
-  __tablename__ = 'items'
+    __tablename__ = 'items'
 
-  id = Column(Integer, primary_key = True)
-  item_name = Column(String)
-  item_price = Column(String)
-  item_description = Column(String)
-  item_image = Column(String)
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    item_price = Column(String)
+    item_description = Column(String)
+    item_image = Column(String)
 
-  category_id = Column(Integer, ForeignKey('categories.id'))
-  category = relationship(Category)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship(Category)
 
-  user_id = Column(Integer, ForeignKey('users.id'))
-  user = relationship(User)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
 
-  @property
-  def serialize(self):
-      #Returns data in easily serializable format
-      return {
-        'item_name' : self.item_name,
-        'item_image'   : self.item_image,
-        'item_price' : self.item_price,
-        'item_description' : self.item_description,
-        'category_id' : self.category_id,
-        'id' : self.id,
-        'user_id' : self.user_id
-      }
+    @property
+    def serialize(self):
+        # Returns data in easily serializable format
+        return {
+            'item_name': self.item_name,
+            'item_image': self.item_image,
+            'item_price': self.item_price,
+            'item_description': self.item_description,
+            'category_id': self.category_id,
+            'id': self.id,
+            'user_id': self.user_id
+          }
 
 engine = create_engine('sqlite:///catalog.db')
 
